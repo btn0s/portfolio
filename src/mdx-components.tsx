4ebcd06c36d100d5441c4b20d8e2c245a70e9785
b <btn0s@users.noreply.github.com>
2025-03-17 12:27:20 -0700

    Add @radix-ui/react-visually-hidden package and enhance Image component with dialog functionality

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,10 +1,34 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 import type { MDXComponents } from "mdx/types";
 import Image from "next/image";
-import { Mermaid } from "mdx-mermaid/lib/Mermaid";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
-    Image: (props) => <Image {...props} />,
+    Image: (props) => (
+      <Dialog>
+        <DialogTrigger>
+          <Image {...props} />
+        </DialogTrigger>
+        <DialogContent className={"p-0 max-w-6xl border-0 bg-transparent"}>
+          <VisuallyHidden>
+            <DialogTitle>Image</DialogTitle>
+          </VisuallyHidden>
+          <Image
+            src={props.src}
+            alt={props.alt}
+            width={1600}
+            height={900}
+            className={"w-screen rounded-md shrink-0 bg-muted border shadow"}
+          />
+        </DialogContent>
+      </Dialog>
+    ),
   };
 }
