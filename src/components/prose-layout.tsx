08c6626f408f8806c0313df4b2f73bb29c95c831
b <btn0s@users.noreply.github.com>
2025-03-16 23:15:54 -0700

    Update ProseLayout component to adjust heading sizes for improved typography

--- src/components/prose-layout.tsx
+++ src/components/prose-layout.tsx
@@ -4,7 +4,7 @@ import { motion } from "motion/react";
 const ProseLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <motion.div
-      className="prose dark:prose-invert prose-sm max-w-none prose-h1:text-2xl prose-h1:font-medium prose-h2:font-medium prose-h3:font-medium prose-h4:font-medium prose-h5:font-medium prose-h6:font-medium"
+      className="prose dark:prose-invert prose-sm max-w-none prose-h1:text-xl prose-h1:font-medium prose-h2:font-medium prose-h3:font-medium prose-h4:font-medium prose-h5:font-medium prose-h6:font-medium"
       variants={VARIANTS_SECTION}
       transition={TRANSITION_SECTION}
     >
